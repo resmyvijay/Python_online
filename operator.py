5ae0327b6dd1314df4e67d0698bc99421124d1d5
Resmy <55789995+resmyvijay@users.noreply.github.com>
2025-07-03 23:58:58 +0800

    Update operator.py

--- operator.py
+++ operator.py
@@ -1,6 +1,5 @@
 # Arithmetic Operations in Python
 # Integers
-
 print('Addition: ', 1 + 2)
 print('Subtraction: ', 2 - 1)
 print('Multiplication: ', 2 * 3)
@@ -11,3 +10,4 @@ print('Division without the remainder: ', 7 // 2)   # gives without the floating
 print('Modulus: ', 3 % 2)                           # Gives the remainder
 print ('Division without the remainder: ', 7 // 3)
 print('Exponential: ', 3 ** 2)                     # it means 3 * 3
+
